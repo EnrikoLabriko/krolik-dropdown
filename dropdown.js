640c5b7b8c0918e03f30a699b0e98a6ad9034bd3
Alexander Telyatnikov <alexandertel@yandex.ru>
2014-12-11 01:45:11 +0300

    Final style fixes

--- dropdown.js
+++ dropdown.js
@@ -1,55 +1,62 @@
-var Dropdown = function (id, options) {
+var Dropdown = function(id, options) {
 
     var dropdownToggleOpen = function() {
         if (getComputedStyle(ul).display == 'none') {
-            ul.style.display = 'block';
-            options.onOpen()
+            dropdownOpen();
         } else {
-            ul.style.display = 'none';
-            options.onClose()
+            dropdownClose();
         }
     };
 
     var dropdownOpen = function() {
-        ul.style.display = 'block';
-        options.onOpen()
+        if (getComputedStyle(ul).display != 'block') {
+            ul.style.display = 'block';
+            if (options.onOpen) {
+                options.onOpen();
+            }
+        }
     };
 
     var dropdownClose = function() {
         if (getComputedStyle(ul).display != 'none') {
             ul.style.display = 'none';
-            options.onClose()
+            if (options.onClose) {
+                options.onClose();
+            }
         }
-
     };
 
-    var dropdownItemClick = function(event) {
+    var dropdownItemClick = function() {
         event.stopPropagation();
+
         var node = event.currentTarget;
-        // We'd like to do something like this:
-        //     var index = node.parentNode.children.indexOf(node);
+
+        // I'd like to do something like this:
+        //     var index = ul.children.indexOf(node);
         // But NodeList doesn't support indexOf() method ¯\_(ツ)_/¯
-        var index = [].indexOf.call(node.parentNode.children, node);
+        var index = [].indexOf.call(ul.children, node);
         if (options.items[index]) {
             options.items[index]()
         }
-        dropdownClose()
+
+        dropdownClose();
     };
 
     var div = document.getElementById(id);
     var ul = div.getElementsByTagName('ul')[0];
     var innerDiv = div.getElementsByTagName('div')[0];
-    if (options.event == 'click') {
+
+    if (!options.event || options.event == 'click') {
         innerDiv.addEventListener('click', dropdownToggleOpen, false);
     } else if (options.event == 'hover') {
         div.addEventListener('mouseenter', dropdownOpen, false);
         div.addEventListener('mouseleave', dropdownClose, false);
-
     }
-    for (i = 0; i < options.items.length; i++) {
-        if (ul.children[i].getAttribute('disabled') === null) {
-            ul.children[i].addEventListener('click', dropdownItemClick, false)
+
+    for (var i = 0; i < options.items.length; i++) {
+        var li = ul.children[i];
+        if (li.getAttribute('disabled') === null) {
+            li.addEventListener('click', dropdownItemClick, false);
         }
     }
-
 };
