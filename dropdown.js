2d944c6e4e144e97156120cc8223ddd8236c90c5
Alexander Telyatnikov <alexandertel@yandex.ru>
2014-12-10 19:38:10 +0300

    Added dropdown item click handler

--- dropdown.js
+++ dropdown.js
@@ -1,25 +1,36 @@
 var Dropdown = function (id, options) {
 
-    var toggleOpen = function () {
-        console.log('ul.style.display:', ul.style.display);
-        console.log('getComputedStyle(ul).display:', getComputedStyle(ul).display);
+    var dropdownToggleOpen = function () {
+        //console.log('ul.style.display:', ul.style.display);
+        //console.log('getComputedStyle(ul).display:', getComputedStyle(ul).display);
         if (getComputedStyle(ul).display == 'none') {
             ul.style.display = 'block';
-            console.log('aaa');
+            //console.log('aaa');
         } else {
             ul.style.display = 'none';
-            console.log('bbb');
+            //console.log('bbb');
+        }
+    };
+
+    var dropdownItemClick = function(event) {
+        event.stopPropagation();
+        if (event.target.classList.contains('dropdown-selected')) {
+            event.target.classList.remove('dropdown-selected')
+        } else {
+            event.target.classList.add('dropdown-selected')
         }
     };
 
     var newDiv = document.createElement('div');
     var div = document.getElementById(id);
     var ul = div.getElementsByTagName('ul')[0];
-    div.addEventListener("click", toggleOpen, false);
-    console.log(options.items.length)
+    div.addEventListener('click', dropdownToggleOpen, false);
+    //console.log(options.items.length);
     for (i = 0; i < options.items.length; i++) {
         var li = document.createElement('li');
         li.innerText = options.items[i];
-        ul.appendChild(li)
+        ul.appendChild(li);
+        li.addEventListener('click', dropdownItemClick, false)
     }
+
 };
