6692cb6e00842f2e1eab42a6ff6449be37353fb3
Alexander Telyatnikov <alexandertel@yandex.ru>
2014-12-11 00:59:54 +0300

    Added two different dropdowns on the page and fixed bugs

--- dropdown.js
+++ dropdown.js
@@ -1,56 +1,52 @@
 var Dropdown = function (id, options) {
 
-    var dropdownToggleOpen = function () {
-        //console.log('ul.style.display:', ul.style.display);
-        //console.log('getComputedStyle(ul).display:', getComputedStyle(ul).display);
+    var dropdownToggleOpen = function() {
         if (getComputedStyle(ul).display == 'none') {
             ul.style.display = 'block';
             options.onOpen()
-            //console.log('aaa');
         } else {
             ul.style.display = 'none';
             options.onClose()
-            //console.log('bbb');
         }
+    };
+
+    var dropdownOpen = function() {
+        ul.style.display = 'block';
+        options.onOpen()
+    };
+
+    var dropdownClose = function() {
+        if (getComputedStyle(ul).display != 'none') {
+            ul.style.display = 'none';
+            options.onClose()
+        }
+
     };
 
     var dropdownItemClick = function(event) {
         event.stopPropagation();
-        //console.log(event.currentTarget);
         var node = event.currentTarget;
         // We'd like to do something like this:
         //     var index = node.parentNode.children.indexOf(node);
         // But NodeList doesn't support indexOf() method ¯\_(ツ)_/¯
         var index = [].indexOf.call(node.parentNode.children, node);
-        //console.log('index: ', index);
-        //console.log(options.items[index][1])
         if (options.items[index]) {
             options.items[index]()
         }
-        if (node.classList.contains('dropdown-selected')) {
-            node.classList.remove('dropdown-selected')
-        } else {
-            node.classList.add('dropdown-selected')
-        }
+        dropdownClose()
     };
 
-    //var newDiv = document.createElement('div');
     var div = document.getElementById(id);
     var ul = div.getElementsByTagName('ul')[0];
     var innerDiv = div.getElementsByTagName('div')[0];
     if (options.event == 'click') {
         innerDiv.addEventListener('click', dropdownToggleOpen, false);
     } else if (options.event == 'hover') {
-        innerDiv.addEventListener('mouseover', dropdownToggleOpen, false);
-        innerDiv.addEventListener('mouseout', dropdownToggleOpen, false);
+        div.addEventListener('mouseenter', dropdownOpen, false);
+        div.addEventListener('mouseleave', dropdownClose, false);
 
     }
-    //console.log(options.items.length);
     for (i = 0; i < options.items.length; i++) {
-        //var li = document.createElement('li');
-        //ul.appendChild(li);
-        //console.log(ul.children)
-        console.log(ul.children[i].getAttribute('disabled'))
         if (ul.children[i].getAttribute('disabled') === null) {
             ul.children[i].addEventListener('click', dropdownItemClick, false)
         }
