b5900df66bc1726540c0f21be9482678cb93c201
Alexander Telyatnikov <alexandertel@yandex.ru>
2014-12-10 15:39:40 +0300

    Added function to create dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,15 +1,19 @@
+var Dropdown = function (id) {
+    var handler = function () {
+        var ul = div.getElementsByTagName('ul')[0];
+        var ulStyleDisplay = ul;
+        console.log('ul.style.display:', ul.style.display);
+        console.log('getComputedStyle(ul).display:', getComputedStyle(ul).display);
+        if (getComputedStyle(ul).display == 'none') {
+            ul.style.display = 'block';
+            console.log('aaa');
+        } else {
+            ul.style.display = 'none';
+            console.log('bbb');
+        }
+    };
 
-
-function Dropdown(id) {
-
-    var newDiv = document.createElement('div')
-    var div = document.getElementById(id)
+    var newDiv = document.createElement('div');
+    var div = document.getElementById(id);
     div.addEventListener("click", handler, false)
-}
-
-var handler = function() {
-    var ul = div.getElementsByTagName(ul)
-    ul.style.display = "block"
-    alert('Trololo')
-
-}
+};
